8c7c542a944adc4544966a7d640af544617f229f
ravimodiya <modiyaravi007@gmail.com>
2025-12-10 23:36:24 +0530

    Delete inventory directory

--- inventory/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.inventory {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.sql;
-
-    opens com.inventory to javafx.fxml;
-    exports com.inventory;
-}
